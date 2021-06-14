0e5833809d127a7f877c2b31c8163ab45a5f5e35
NickFleece <nebf97@gmail.com>
2021-06-14 02:31:18 -0400

    make multithreading

--- scripts/combine_openpose.py
+++ scripts/combine_openpose.py
@@ -4,25 +4,45 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
+import threading
+from queue import Queue
+import time
 
+MAX_THREADS = 3
 pbar = tqdm(total=928)
+q = Queue()
+
+#name is j
+#hmap_folder is heatmaps_folder
+def createnpz(name, hmap_folder):
+    all_heatmaps = []
+    heatmap_index = 0
+    while (os.path.exists(hmap_folder + "/" + name + "_" + str(heatmap_index).zfill(12) + "_pose_heatmaps.png")):
+        all_heatmaps.append(
+            plt.imread(hmap_folder + "/" + name + "_" + str(heatmap_index).zfill(12) + "_pose_heatmaps.png"))
+        heatmap_index += 1
+
+    with open(hmap_folder + "/" + name + ".npy", 'wb') as f:
+        np.savez_compressed(f, np.asarray(all_heatmaps))
+
+    q.task_done()
+
+tasks_in_progress = 0
 for i in os.listdir(DATA_PATH):
     pbar.set_description(i)
     class_folder_path = DATA_PATH + "/" + i
     
     for j in os.listdir(class_folder_path):
         heatmaps_folder = class_folder_path + "/" + j
-        
-        all_heatmaps = []
-        heatmap_index = 0
-        while(os.path.exists(heatmaps_folder + "/" + j + "_" + str(heatmap_index).zfill(12) + "_pose_heatmaps.png")):
-            all_heatmaps.append(plt.imread(heatmaps_folder + "/" + j + "_" + str(heatmap_index).zfill(12) + "_pose_heatmaps.png"))
-            heatmap_index += 1
 
-        with open(heatmaps_folder + "/" + j + ".npy", 'wb') as f:
-            np.save(f, np.asarray(all_heatmaps))
-        
-        # with open(heatmaps_folder + "/" + j + ".npy", 'rb') as f:
-        #     np.load(f)
+        if tasks_in_progress >= MAX_THREADS:
+            q.join()
+            tasks_in_progress = 0
 
+        t = threading.Thread(target=createnpz, args=(j,heatmaps_folder))
+        q.put(t)
+        t.start()
+        tasks_in_progress += 1
+
+        time.sleep(0.5)
         pbar.update(1)
