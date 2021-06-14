21442a6ae96dd28c1e3c163343a92fcb00b4a908
NickFleece <nebf97@gmail.com>
2021-06-14 14:43:08 -0400

    multithread the data

--- potion.py
+++ potion.py
@@ -23,6 +23,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, BatchNormalization, GlobalAveragePooling2D, Dropout
 from tensorflow.keras.metrics import categorical_accuracy
 from tensorflow.keras.optimizers import Adam
+import queue
+import threading
 
 physical_devices = tf.config.list_physical_devices('GPU')
 print("Num GPUs:", len(physical_devices))
@@ -89,12 +91,14 @@ data = pd.DataFrame(data)
 
 class DataGenerator(tf.keras.utils.Sequence):
 
-    def __init__(self, df, ind, batch_size=8, verbose=False):
+    def __init__(self, df, ind, batch_size=8):
         self.seed = RANDOM_SEED
         self.df_train = df.loc[df['ind'] == ind].loc[df['split'] == 'train'].sample(random_state=self.seed, frac=1)
         self.df_test = df.loc[df['ind'] == ind].loc[df['split'] == 'test'].sample(random_state=self.seed, frac=1)
         self.batch_size = batch_size
-        self.verbose = verbose
+
+        self.job_queue = queue.Queue()
+        self.result_queue = queue.Queue()
 
         self.target_class_map = [
             'kick_ball',
@@ -151,87 +155,96 @@ class DataGenerator(tf.keras.utils.Sequence):
     def __len__(self):
         return len(self.df_train) // self.batch_size
 
+    def getDataItem(self, f, c):
+        openpose_heatmaps_dir = f"{DATA_PATH}/OpenPose_Heatmaps/{c}/{f[:-4]}"
+
+        with open(f"{openpose_heatmaps_dir}/{f[:-4]}.npy", 'rb') as f:
+            all_heatmaps = np.load(f)
+
+        slice_size = int(np.asarray(all_heatmaps).shape[2] / 25)
+
+        U = []
+        I = []
+        N = []
+
+        for i in range(len(self.parts)):
+
+            part_heatmaps = []
+            for heatmap in all_heatmaps:
+                part_map = heatmap[:, slice_size * i:slice_size * i + slice_size]
+                part_heatmaps.append(part_map)
+
+            channels = 3
+            concat_heatmap = np.zeros((part_heatmaps[0].shape[0], part_heatmaps[0].shape[1], channels))
+
+            for j in range(len(all_heatmaps)):
+                part_map = part_heatmaps[j]
+
+                t = (j + 1) / len(all_heatmaps)
+
+                for k in range(channels):
+                    step = (1 / (channels - 1)) * k
+                    channel_map = part_map * max((-(channels - 1) * (abs(t - step)) + 1), 0)
+                    concat_heatmap[:, :, k] += channel_map
+
+            for k in range(channels):
+                concat_heatmap[:, :, k] /= concat_heatmap.max()
+            Uj = concat_heatmap
+            U.append(concat_heatmap)
+
+            Ij = np.sum(Uj, axis=2)
+            I.append(Ij)
+            i3 = np.reshape(Ij, (Ij.shape[0], Ij.shape[1], 1))
+            i3 = np.repeat(i3, 3, axis=2)
+            Nj = Uj / (i3 + 1)
+            N.append(Nj)
+
+        U = np.asarray(U)
+        I = np.asarray(I)
+        N = np.asarray(N)
+
+        single_result_arr = []
+
+        # go from (x,x,3) to (3,x,x), same idea for the others, just stacking all channels
+        for u in U:
+            for j in range(3):
+                single_result_arr.append(u[:, :, j])
+
+        for i in I:
+            single_result_arr.append(i)
+
+        for n in N:
+            for j in range(3):
+                single_result_arr.append(n[:, :, j])
+
+        single_target = np.zeros(len(self.target_class_map))
+        single_target[self.target_class_map.index(c)] = 1
+
+        self.result_queue.put((single_target, single_result_arr))
+        self.job_queue.task_done()
+
     def __getitem__(self, index):
         batch_df = self.df_train.iloc[index * self.batch_size: (index + 1) * self.batch_size]
 
         result_arr = []
         target = []
 
-        if self.verbose:
-            pbar = tqdm(total=self.batch_size, desc=f"Batch {index} / {self.__len__()}")
-
         for i in batch_df.iterrows():
             f = i[1]['file']
             c = i[1]['class']
 
-            openpose_heatmaps_dir = f"{DATA_PATH}/OpenPose_Heatmaps/{c}/{f[:-4]}"
-            
-            with open(f"{openpose_heatmaps_dir}/{f[:-4]}.npy", 'rb') as f:
-                all_heatmaps = np.load(f)
+            #MULTITHREADING HERE
+            t = threading.Thread(target=self.getDataItem, args=(f,c))
+            t.start()
+            self.job_queue.put(t)
 
-            slice_size = int(np.asarray(all_heatmaps).shape[2]/25)
+        self.job_queue.join()
 
-            U = []
-            I = []
-            N = []
+        while not self.result_queue.empty():
+            single_target, single_result_arr = self.result_queue.get()
 
-            for i in range(len(self.parts)):
-
-                part_heatmaps = []
-                for heatmap in all_heatmaps:
-                    part_map = heatmap[:, slice_size * i:slice_size * i + slice_size]
-                    part_heatmaps.append(part_map)
-
-                channels = 3
-                concat_heatmap = np.zeros((part_heatmaps[0].shape[0], part_heatmaps[0].shape[1], channels))
-
-                for j in range(len(all_heatmaps)):
-                    part_map = part_heatmaps[j]
-
-                    t = (j + 1) / len(all_heatmaps)
-
-                    for k in range(channels):
-                        step = (1 / (channels - 1)) * k
-                        channel_map = part_map * max((-(channels - 1) * (abs(t - step)) + 1), 0)
-                        concat_heatmap[:, :, k] += channel_map
-
-                for k in range(channels):
-                    concat_heatmap[:, :, k] /= concat_heatmap.max()
-                Uj = concat_heatmap
-                U.append(concat_heatmap)
-
-                Ij = np.sum(Uj, axis=2)
-                I.append(Ij)
-                i3 = np.reshape(Ij, (Ij.shape[0], Ij.shape[1], 1))
-                i3 = np.repeat(i3, 3, axis=2)
-                Nj = Uj/(i3+1)
-                N.append(Nj)
-
-            U = np.asarray(U)
-            I = np.asarray(I)
-            N = np.asarray(N)
-
-            single_result_arr = []
-
-            # go from (x,x,3) to (3,x,x), same idea for the others, just stacking all channels
-            for u in U:
-                for j in range(3):
-                    single_result_arr.append(u[:, :, j])
-
-            for i in I:
-                single_result_arr.append(i)
-
-            for n in N:
-                for j in range(3):
-                    single_result_arr.append(n[:, :, j])
             result_arr.append(single_result_arr)
-
-            single_target = np.zeros(len(self.target_class_map))
-            single_target[self.target_class_map.index(c)] = 1
             target.append(single_target)
-
-            if self.verbose:
-                pbar.update(1)
 
         result_arr = np.asarray(result_arr)
         target = np.asarray(target)
@@ -242,7 +255,7 @@ class DataGenerator(tf.keras.utils.Sequence):
         self.df_train = self.df_train.sample(random_state=self.seed, frac=1)
         # self.df_test = self.df_test.sample(random_state=self.seed, frac=1)
 
-dg = DataGenerator(data, 1, batch_size=20, verbose=True)
+dg = DataGenerator(data, 1, batch_size=20)
 #print(dg.__getitem__(0))
 
 model_init = tf.keras.initializers.GlorotNormal(seed=RANDOM_SEED)
