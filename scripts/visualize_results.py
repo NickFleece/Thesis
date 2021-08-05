331320100dec4a6d9acecdcf2009feb4669149be
NickFleece <nebf97@gmail.com>
2021-08-05 19:11:44 -0400

    things

--- scripts/visualize_results.py
+++ scripts/visualize_results.py
@@ -1,13 +1,13 @@
 import pickle
 import matplotlib.pyplot as plt
 
-with open("../models/pa3d_torch_model_hist.pickle", 'rb') as f:
+with open("../potion/models/tf_test_model_hist.pickle", 'rb') as f:
     data = pickle.load(f)
 
 plt.figure(figsize=(15,15))
 for k in data.keys():
     plt.plot(range(len(data[k])), data[k], label=k)
 plt.legend()
-plt.show()
-
+#plt.show()
+plt.savefig("graph.png")
 # print(data.keys())
