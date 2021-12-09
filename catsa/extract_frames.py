15a6c23ef71bde98cb4fa4ff517c8b679c069e08
NickFleece <nebf97@gmail.com>
2021-12-09 18:09:27 -0500

    update extract frames file

--- catsa/extract_frames.py
+++ catsa/extract_frames.py
@@ -1,39 +1,64 @@
 import os
 import shutil
 import cv2
-from tqdm import tqdm
-import numpy as np
-import matplotlib.pyplot as plt
+import argparse
+import time
 
-#VIDEO_FOLDER = "D:/CATSA/CATSA_DATA_SEGMENTED"
-VIDEO_FOLDER = "/media/nick/External Boi/CATSA/CATSA_DATA_SEGMENTED"
+parser = argparse.ArgumentParser()
+parser.add_argument('--env', required=True)
+parser.add_argument('--max_threads', default=1)
+args = parser.parse_args()
 
-pbar = tqdm()
+ALL_VIDEO_FOLDERS = {
+    "home":"H:/CATSA/CATSA_DATA_SEGMENTED",
+    "laptop":"/media/nick/Thesis/CATSA/CATSA_DATA_SEGMENTED"
+}
+VIDEO_FOLDER = ALL_VIDEO_FOLDERS[args.env]
 
+ALL_EXPORT_DIR = {
+    "home":"H:/CATSA/CATSA_FRAMES"
+}
+EXPORT_DIR = ALL_EXPORT_DIR[args.env]
+
+save_format='jpg'
+
+total_videos = 0
 for date in os.listdir(VIDEO_FOLDER):
+    total_videos += len(os.listdir(f"{VIDEO_FOLDER}/{date}"))
+
+curr_file = 0
+threads = []
+for date in os.listdir(VIDEO_FOLDER):
+
     for video_file in os.listdir(f"{VIDEO_FOLDER}/{date}"):
+
+        curr_file += 1
 
         #skip any existing folders (we only want the video files)
         if not ".wmv" in video_file:
             continue
 
-        FRAMES_PATH = f"{VIDEO_FOLDER}/{date}/{video_file[:-4]}"
+        frames_path = f"{EXPORT_DIR}/{date}/{video_file[:-4]}"
+        video_file_path = f"{VIDEO_FOLDER}/{date}/{video_file}"
         
         #delete a folder if it already exists (basically just create a new empty one)
-        if os.path.exists(FRAMES_PATH):
-            shutil.rmtree(FRAMES_PATH)
+        if os.path.exists(frames_path):
+            shutil.rmtree(frames_path)
 
         #make the new dir to put the frames
-        os.mkdir(FRAMES_PATH)
+        os.mkdir(frames_path)
+    
+        print(f"{video_file_path}, video {curr_file} / {total_videos}")
+
+        start_time = time.time()
 
         vidcap = cv2.VideoCapture(f"{VIDEO_FOLDER}/{date}/{video_file}")
-        
         frame_read_success, frame = vidcap.read()
         frame_count = 0
         while frame_read_success:
-            cv2.imwrite(f"{FRAMES_PATH}/{str(frame_count).zfill(5)}.jpg", frame)
+            cv2.imwrite(f"{frames_path}/{str(frame_count).zfill(5)}.{save_format}", frame)
             frame_count += 1
             frame_read_success, frame = vidcap.read()
-            pbar.set_description(f"{date}, {video_file}, {str(frame_count).zfill(5)}")
+            print(frame_count, end='\r')
         
-        pbar.update(1)
+        print(f"\nTook: {time.time() - start_time}, Left: {(time.time() - start_time) * (total_videos - (curr_file + 1))}\n")
