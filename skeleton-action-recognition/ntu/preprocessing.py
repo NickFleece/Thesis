c8f0be53203306bf3575e3f9fb8c2a982013fd01
NickFleece <nebf97@gmail.com>
2022-01-23 20:36:38 -0500

    things

--- skeleton-action-recognition/ntu/preprocessing.py
+++ skeleton-action-recognition/ntu/preprocessing.py
@@ -2,8 +2,20 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 
-SKELETON_FILES_DIR = "H:/nturgbd_skeletons_s001_to_s017/nturgb+d_skeletons"
-VIDEO_FILES_DIR = "H:/ntu_dataset/nturgb+d_rgb"
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--drive_dir', required=True)
+args = parser.parse_args()
+
+BASE_DIR = args.drive_dir
+
+SKELETON_FILES_DIR = f"{BASE_DIR}/nturgbd_skeletons_s001_to_s017/nturgb+d_skeletons"
+VIDEO_FILES_DIR = f"{BASE_DIR}/ntu_dataset/nturgb+d_rgb"
+NEW_EXPORT_DIR = f"{BASE_DIR}/processed_skeleton_data"
+
+print(SKELETON_FILES_DIR)
+print(VIDEO_FILES_DIR)
 
 for i in os.listdir(SKELETON_FILES_DIR):
 
@@ -11,8 +23,8 @@ for i in os.listdir(SKELETON_FILES_DIR):
 
     _, frame = cap.read()
 
-    plt.imshow(frame)
-    plt.show()
+    # plt.imshow(frame)
+    # plt.show()
 
     with open(f"{SKELETON_FILES_DIR}/{i}") as f:
         num_frames = int(f.readline())
@@ -31,7 +43,7 @@ for i in os.listdir(SKELETON_FILES_DIR):
                 skeleton_points = []
 
                 for _ in range(num_points):
-
+                    
                     point = f.readline().split(" ")[:2]
                     for p in range(len(point)): point[p] = float(point[p])
                     plt.scatter(point[0], point[1])
