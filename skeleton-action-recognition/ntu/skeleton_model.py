dd35706bf6a3c068d1668aa86a2a204450dccdfe
NickFleece <nebf97@gmail.com>
2022-02-19 17:16:33 -0500

    increase batch size, lower lr

--- skeleton-action-recognition/ntu/skeleton_model.py
+++ skeleton-action-recognition/ntu/skeleton_model.py
@@ -79,9 +79,9 @@ for json_path in all_data_files:
 X_train, X_test, y_train, y_test = train_test_split(data, classes, test_size=0.2, random_state=RANDOM_STATE, stratify=classes)
 
 # HYPERPARAMETERS:
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0001
 EPOCHS = 100
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cpu = torch.device("cpu")
