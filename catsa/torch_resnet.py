e76b74de20a0b9592a0f51ad5b23eac2f61868d0
NickFleece <nebf97@gmail.com>
2022-03-06 19:29:16 -0500

    oop

--- catsa/torch_resnet.py
+++ catsa/torch_resnet.py
@@ -219,10 +219,10 @@ for e in range(EPOCHS):
             model_out = []
             for sample in batch_samples:
                 model_out.append(model(sample.to(device)))
-            torch.cat(model_out)
+            model_out = torch.cat(model_out)
 
-            print(model_out.shape)
-            print(model_out)
+            # print(model_out.shape)
+            # print(model_out)
 
             loss = criterion(
                 model_out,
