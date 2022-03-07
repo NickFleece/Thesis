27cebcdbc74eb0a5082b6161fedfec10b73ebc19
NickFleece <nebf97@gmail.com>
2022-03-06 20:44:44 -0500

    Update torch_resnet.py

--- catsa/torch_resnet.py
+++ catsa/torch_resnet.py
@@ -164,16 +164,7 @@ class VideoRecognitionModel(nn.Module):
 
 model = VideoRecognitionModel()
 model.to(device)
-
-os.environ['MASTER_ADDR'] = 'localhost'
-os.environ['MASTER_PORT'] = '12355'
-torch.distributed.init_process_group(
-    	backend='nccl',
-   		init_method='env://',
-    	world_size=2,
-    	rank=0
-    )
-model = nn.parallel.DistributedDataParallel(model)
+model = nn.DataParallel(model)
 
 # torch.distributed.init_process_group('nccl')
 # model = nn.parallel.DistributedDataParallel(model, device_ids=[0], output_device=1)
