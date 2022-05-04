148c1205fcc33f1662602779e5c2ca50a719ed71
NickFleece <nebf97@gmail.com>
2022-05-04 16:44:22 -0400

    Update skeleton_model.py

--- skeleton-action-recognition/ntu/skeleton_model.py
+++ skeleton-action-recognition/ntu/skeleton_model.py
@@ -54,7 +54,7 @@ def load_data(json_path):
         print("Something has gone wrong? More than one skeleton.")
         return
     final_json = person_combined_skeleton_json[0]
-    print(max(final_json))
+    print(np.max(final_json))
 
     # channel last to channel first
     channel_first_final_json = []
