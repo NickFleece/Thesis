b002c64ba3356f0958c114debef68a04f3612a2e
NickFleece <nebf97@gmail.com>
2022-06-09 02:43:26 -0400

    Create data_check.py

---
+++ skeleton-action-recognition/fast-food/data_check.py
@@ -0,0 +1,32 @@
+import json
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import argparse
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import time
+
+RANDOM_STATE = 42
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--drive_dir', required=True)
+args = parser.parse_args()
+
+BASE_DIR = args.drive_dir
+
+data_summary = pd.read_csv(f"{BASE_DIR}/data_summary.csv")
+data_summary = data_summary.fillna("None")
+
+max_len = 0
+
+for _, d in data_summary.iterrows():
+
+    with open(f"{BASE_DIR}/{d['folder']}/extracted_pose/{d['category']}~{d['instance_id']}~{d['person_id']}.json", 'r') as f:
+        skeleton_data = np.asarray(json.load(f))
+
+    print(skeleton_data.shape)
+    break
