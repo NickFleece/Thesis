241f619fb9f51e2edc7a2dcb9fb4eaa932b5784c
NickFleece <nebf97@gmail.com>
2022-06-15 15:21:53 -0400

    some metric calculations

---
+++ skeleton-action-recognition/fast-food/results_analysis.py
@@ -0,0 +1,30 @@
+import os
+import argparse
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+import torch
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--results_dir', required=True)
+args = parser.parse_args()
+
+RESULTS_DIR = args.results_dir
+
+print("Starting results parse...")
+for folder in os.listdir(RESULTS_DIR):
+    print(f"\n Version: {folder}")
+
+    max_acc = 0.0
+    for result_file in os.listdir(f"{RESULTS_DIR}/{folder}"):
+
+        result = torch.load(f"{RESULTS_DIR}/{folder}/{result_file}")
+
+        acc = accuracy_score(result['val_actual'], result['val_predicted'])
+        if acc > max_acc:
+
+            max_acc = acc
+            max_prec = precision_score(result['val_actual'], result['val_predicted'])
+            max_rec = recall_score(result['val_actual'], result['val_predicted'])
+            conf_matrix = confusion_matrix(result['val_actual'], result['val_predicted'])
+        
+        break
+    break
