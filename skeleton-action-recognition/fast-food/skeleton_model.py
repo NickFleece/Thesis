f9639dcdd9ad8b9b3b071dbeb4207b1c538067c3
NickFleece <nebf97@gmail.com>
2022-08-12 00:04:57 -0400

    Update skeleton_model.py

--- skeleton-action-recognition/fast-food/skeleton_model.py
+++ skeleton-action-recognition/fast-food/skeleton_model.py
@@ -1,7 +1,7 @@
 #HYPERPARAMETERS:
 LEARNING_RATE = 0.1
 EPOCHS = 500
-BATCH_SIZE = 8
+BATCH_SIZE = 16
 MAX_FRAMES = 881
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
@@ -139,10 +139,10 @@ class CNN(nn.Module):
         self.fc = nn.Sequential(
             nn.AdaptiveAvgPool2d((1,1)),
             nn.Flatten(),
-            nn.Dropout(0.7),
+            nn.Dropout(),
             nn.Linear(512,512),
             nn.ReLU(),
-            nn.Dropout(0.7),
+            nn.Dropout(),
             nn.Linear(512, len(categories)),
             nn.Softmax(dim=1)
         )
@@ -175,8 +175,7 @@ optimizer = optim.SGD(
 )
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(
     optimizer,
-    factor=0.5, 
-    patience=25
+    factor=0.1
 )
 
 train_accuracies = []
