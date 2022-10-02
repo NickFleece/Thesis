2ef358a1380b4d2c2720ca8856b1c3270f7d4d5f
NickFleece <nebf97@gmail.com>
2022-10-02 17:11:45 -0400

    fix results script

--- skeleton-action-recognition/ntu/displayresults.py
+++ skeleton-action-recognition/ntu/displayresults.py
@@ -1,12 +1,42 @@
+import os
 import torch
 import argparse
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file_dir', required=True)
+parser.add_argument('--results_dir', required=True)
 args = parser.parse_args()
 
-FILE_DIR = args.file_dir
+RESULTS_DIR = args.results_dir
 
-results = torch.load(FILE_DIR, map_location=torch.device('cpu'))
-print(results['val_predicted'][:10])
-print(results['val_actual'][:10])
+accuracies = {}
+
+print("starting...")
+for folder in os.listdir(RESULTS_DIR):
+    print(f"\n Version: {folder}")
+
+    max_acc = 0.0
+    for result_file in os.listdir(f"{RESULTS_DIR}/{folder}"):
+
+        result = torch.load(f"{RESULTS_DIR}/{folder}/{result_file}", map_location=torch.device('cpu'))
+
+        acc = accuracy_score(result['val_actual'], result['val_predicted'])
+        if acc > max_acc:
+
+            max_acc = acc
+            max_prec = precision_score(result['val_actual'], result['val_predicted'], average="micro")
+            max_rec = recall_score(result['val_actual'], result['val_predicted'], average="micro")
+            conf_matrix = confusion_matrix(result['val_actual'], result['val_predicted'])
+        
+    print(f"Max Accuracy: {max_acc}")
+    print(f"Max Precision: {max_acc}")
+    print(f"Max Recall: {max_rec}")
+    print(conf_matrix)
+
+    accuracies[folder] = max_acc
+
+print("---")
+print("Sorted:")
+sorted = {k: v for k, v in sorted(accuracies.items(), key=lambda item: item[1])}
+for k in sorted.keys():
+    print(f"{k} - {sorted[k]}")
