e5f426eb7310986c35f0d9e571c8f5facd8941aa
NickFleece <nebf97@gmail.com>
2022-11-21 19:15:03 -0500

    Update skeleton_model.py

--- skeleton-action-recognition/jhmdb/skeleton_model.py
+++ skeleton-action-recognition/jhmdb/skeleton_model.py
@@ -25,39 +25,292 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--drive_dir', required=True)
 parser.add_argument('--load_checkpoint')
 parser.add_argument('--version', required=True)
+parser.add_argument('--split', default=1)
 args = parser.parse_args()
 
 BASE_DIR = args.drive_dir
 VERSION = args.version
+SPLIT = args.split
 
 MODEL_SAVE_DIR = f"{BASE_DIR}/models"
 
 PROCESSED_JOINT_DATA_FOLDER = f"{BASE_DIR}/processed_joints" 
-SPLITS_FOLDER = f"{BASE_DIR}"
+SPLITS_FOLDER = f"{BASE_DIR}/splits/splits"
 
 if not os.path.isdir(f"{MODEL_SAVE_DIR}/m_{VERSION}"):
     os.mkdir(f"{MODEL_SAVE_DIR}/m_{VERSION}")
 
 categories = os.listdir(PROCESSED_JOINT_DATA_FOLDER)
 
-print(f"categories: {categories}")
+print(f"\nCategories: {categories}")
 
 train_split = []
 test_split = []
 
-for i 
-
-x_data = []
-y_data = []
-
 for c in categories:
 
-    print(c)
+    splits_file = f"{SPLITS_FOLDER}/{c}_test_split{SPLIT}.txt"
+    
+    with open(splits_file, 'r') as f:
+        splits_lines = f.readlines()
+    
+    for l in splits_lines:
+
+        split_l = l.split(' ')
+        
+        if split_l[1][0] == '1':
+            train_split.append(split_l[0][:-4])
+        elif split_l[1][0] == '2':
+            test_split.append(split_l[0][:-4])
+        else:
+            print("SOMETHING WRONG")
+            print(split_l)
+
+print("\nSplits")
+print(f"Train: {len(train_split)}")
+print(f"Test: {len(test_split)}")
+
+X_train = []
+y_train = []
+
+X_test = []
+y_test = []
+
+print("\nLoading data...")
+
+for c in categories:
 
     for i in os.listdir(f"{PROCESSED_JOINT_DATA_FOLDER}/{c}"):
 
         with open(f"{PROCESSED_JOINT_DATA_FOLDER}/{c}/{i}", 'r') as f:
-            data = json.load(f)
-            x_data.append(np.asarray(data))
-        y_data.append(categories.index(c))
+            data = np.asarray(json.load(f))
 
+        print(data.shape)
+
+        channel_first_data = []
+        for j in range(data.shape[2]):
+            channel_first_data.append(data[:,:,j])
+        
+        data = np.asarray(channel_first_data)
+
+        if i[:-5] in train_split:
+            X_train.append(data)
+            y_train.append(categories.index(c))
+        elif i[:-5] in test_split:
+            X_test.append(data)
+            y_test.append(categories.index(c))
+        else:
+            print("SOMETHING WRONG")
+            print(i[:-5])
+
+print("Done loading!\n")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+cpu = torch.device("cpu")
+print(f"Running on: {device}")
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(10, 128, kernel_size=(3,3)),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+        )
+
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=(3,3)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+        )
+
+        self.conv_block_3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=(3,3)),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+        )
+
+        self.fc = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+            nn.Dropout(),
+            nn.Linear(512,512),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(512, len(categories)),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, i):
+        
+        x = self.conv_block_1(i)
+        x = self.conv_block_2(x)
+        x = self.conv_block_3(x)
+        x = self.fc(x)
+
+        return x
+
+cnn_net = CNN()
+if device != cpu:
+    cnn_net = nn.DataParallel(cnn_net)
+
+checkpoint = args.load_checkpoint
+if not checkpoint is None:
+    cnn_net.load_state_dict(torch.load(f"{MODEL_SAVE_DIR}/m_{VERSION}/{checkpoint}")['model_state_dict'])
+else:
+    checkpoint = 0
+
+cnn_net.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(
+    cnn_net.parameters(),
+    lr=LEARNING_RATE,
+    momentum=0.9
+)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+    optimizer, 
+    factor=0.5,
+    patience=25
+)
+
+train_accuracies = []
+val_accuracies = []
+
+for e in range(int(checkpoint), EPOCHS):
+
+    print(f"Learning rate: {optimizer.param_groups[0]['lr']}")
+
+    #shuffle dataset
+    X_train, y_train = shuffle(X_train, y_train, random_state=RANDOM_STATE)
+
+    losses = []
+    train_correct = 0
+    train_total = 0
+
+    batch_input = []
+    batch_actual = []
+
+    train_predicted = []
+    train_actual = []
+    val_predicted = []
+    val_actual = []
+
+    pbar = tqdm(total=len(X_train))
+    optimizer.zero_grad()
+
+    for X, y in zip(X_train, y_train):
+
+        pbar.update(1)
+
+        batch_input.append(X)
+        batch_actual.append(y)
+
+        if len(batch_input) == BATCH_SIZE:
+            input_tensor = torch.from_numpy(np.asarray(batch_input)).float().to(device)
+            batch_predicted = cnn_net(input_tensor)
+
+            loss = criterion(
+                batch_predicted,
+                torch.tensor(batch_actual).to(device).long())
+            loss.backward()
+            optimizer.step()
+
+            losses.append(loss.item())
+
+            for output, label in zip(batch_predicted.argmax(dim=1).cpu().detach().numpy(), batch_actual):
+                if output == label:
+                    train_correct += 1
+                train_total += 1
+
+                train_predicted.append(output)
+                train_actual.append(label)
+            
+            pbar.set_description(f"{(train_correct / train_total) * 100}% Correct :)")
+
+            batch_input = []
+            batch_actual = []
+
+            optimizer.zero_grad()
+
+    if len(batch_input) != 0:
+        input_tensor = torch.from_numpy(np.asarray(batch_input)).float().to(device)
+        batch_predicted = cnn_net(input_tensor)
+
+        loss = criterion(
+            batch_predicted,
+            torch.tensor(batch_actual).to(device).long())
+        loss.backward()
+        optimizer.step()
+
+        losses.append(loss.item()) 
+
+        for output, label in zip(batch_predicted.argmax(dim=1).cpu().detach().numpy(), batch_actual):
+            if output == label:
+                train_correct += 1
+            train_total += 1
+
+            train_predicted.append(output)
+            train_actual.append(label)
+        
+        pbar.set_description(f"{(train_correct / train_total) * 100}% Training Correct :)")
+
+    pbar.close()
+
+    train_accuracies.append(train_correct / train_total)
+    print(f"Epoch {e} Loss: {sum(losses) / len(losses)}, Accuracy: {train_correct / train_total}")
+
+    with torch.no_grad():
+
+        val_correct = 0
+
+        pbar = tqdm(total=len(X_test))
+        count = 0
+
+        val_loss = 0
+
+        for X, y in zip(X_test, y_test):
+
+            pbar.update(1)
+
+            input_tensor = torch.from_numpy(np.asarray([X])).float()
+
+            out = cnn_net(input_tensor)
+            pred = out.argmax(dim=1).item()
+
+            val_loss += criterion(out, torch.tensor([y]).to(device).long())
+
+            if pred == y:
+                val_correct += 1
+            
+            val_predicted.append(pred)
+            val_actual.append(y)
+            
+            count += 1
+            pbar.set_description(f"{(val_correct / count) * 100}% Validation Correct :)")
+
+        scheduler.step(val_loss/len(X_test))
+
+        pbar.close()
+        time.sleep(1)
+
+        val_accuracies.append(val_correct / len(y_test))
+        print(f"Epoch {e} Validation Accuracy: {val_correct / len(y_test)}")
+
+
+    print(confusion_matrix(val_actual, val_predicted))
+
+    print("---------------------------------------------------------------")
+
+    torch.save({
+        'epoch': e,
+        'model_state_dict': cnn_net.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': losses,
+        'train_predicted': train_predicted,
+        'train_actual': train_actual,
+        'val_predicted': val_predicted,
+        'val_actual': val_actual,
+        'categories': categories
+    }, f"{MODEL_SAVE_DIR}/m_{VERSION}/{e}")
