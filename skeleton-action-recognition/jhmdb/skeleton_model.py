c219d8a8435872db136cdd3fdc3a791d440de1f7
NickFleece <nebf97@gmail.com>
2023-02-04 02:58:58 -0500

    5.6

--- skeleton-action-recognition/jhmdb/skeleton_model.py
+++ skeleton-action-recognition/jhmdb/skeleton_model.py
@@ -133,25 +133,16 @@ class CNN(nn.Module):
             nn.Conv2d(1, 128, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=(1,3), padding=(0,1)),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
         )
 
         self.conv_block_2 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(256),
             nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=(1,3), padding=(0,1)),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
         )
 
         self.conv_block_3 = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size=(1,3), padding=(0,1)),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Conv2d(512, 512, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(512),
             nn.ReLU(),
         )
