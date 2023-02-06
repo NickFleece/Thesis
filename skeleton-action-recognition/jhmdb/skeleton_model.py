2bb4dec228f510609895c42333819a70de89096c
NickFleece <nebf97@gmail.com>
2023-02-06 00:31:03 -0500

    5.4.1

--- skeleton-action-recognition/jhmdb/skeleton_model.py
+++ skeleton-action-recognition/jhmdb/skeleton_model.py
@@ -130,43 +130,43 @@ class CNN(nn.Module):
         self.conv_block_1 = nn.Sequential(
             nn.Conv2d(10, 128, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(128),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(128, 128, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(128),
-            nn.ReLU(),
+            nn.LeakyReLU(),
         )
 
         self.conv_block_2 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(256),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(256, 256, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(256),
-            nn.ReLU(),
+            nn.LeakyReLU(),
         )
 
         self.conv_block_3 = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(512),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Conv2d(512, 512, kernel_size=(1,3), padding=(0,1)),
             nn.BatchNorm2d(128),
-            nn.ReLU(),
+            nn.LeakyReLU(),
         )
 
         self.vertical_convolutions = nn.Sequential(
             nn.Conv2d(512, 1024, kernel_size=(10,1)),
             nn.BatchNorm2d(1024),
-            nn.ReLU(),
+            nn.LeakyReLU(),
         )
 
         self.fc = nn.Sequential(
             nn.Flatten(),
             nn.Linear(1024*39,1024),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Dropout(),
             nn.Linear(1024,512),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.Linear(512, len(categories)),
             nn.Softmax(dim=1)
         )
