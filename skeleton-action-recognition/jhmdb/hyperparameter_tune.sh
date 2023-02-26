9ba898823ab0b217a5f236194af7c24dab04412f
NickFleece <nebf97@gmail.com>
2023-02-26 01:44:23 -0500

    fix script

--- skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
+++ skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 LEARNING_RATES=('0.1' '0.01' '0.001' '0.0001' '0.00001' '0.000001')
 BATCH_SIZES=('8' '16' '32' '64' '128' '256')
 
@@ -5,7 +7,6 @@ for lr in "${LEARNING_RATES[@]}"
 do
     for batchsize in "${BATCH_SIZES[@]}"
     do
-
 
         python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate $lr --batch_size $batchsize --version $1-$lr-$batchsize
 
