8c974cfccba0080b06fbdee227ed1027f791c94e
NickFleece <nebf97@gmail.com>
2023-03-06 22:51:19 -0500

    update eval script

--- skeleton-action-recognition/jhmdb/quick_eval.py
+++ skeleton-action-recognition/jhmdb/quick_eval.py
@@ -4,27 +4,26 @@ import os
 from sklearn.metrics import accuracy_score, confusion_matrix
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+import time
+import thread
 
 result_dir = "/comm_dat/nfleece/JHMDB/models"
 
 print(f"Analyzing {len(os.listdir(result_dir))} total models...")
 results = {}
 count = 0
-for m in os.listdir(f"{result_dir}"):
 
-    count += 1
-    print(f"{count} - {m}")
-
+def readModelData(modelName):
     max_acc = 0
     max_train_acc = 0
 
     max_e = None
 
-    for e in os.listdir(f"{result_dir}/{m}"):
+    for e in os.listdir(f"{result_dir}/{modelName}"):
 
         if e == 'model': continue
 
-        res = torch.load(f"{result_dir}/{m}/{e}", map_location=torch.device('cpu'))
+        res = torch.load(f"{result_dir}/{modelName}/{e}", map_location=torch.device('cpu'))
         acc = accuracy_score(res['val_actual'], res['val_predicted'])
         train_acc = accuracy_score(res['train_actual'], res['train_predicted'])
 
@@ -36,11 +35,39 @@ for m in os.listdir(f"{result_dir}"):
 
     results[m] = max_acc
 
-    print(f"Epoch {max_e}")
-    print(f"Val Accuracy: {max_acc}")
-    print(f"Train Accuracy: {max_train_acc}")
+for m in os.listdir(f"{result_dir}"):
 
-    print("____________________________")
+    count += 1
+    print(f"{count} - {m}")
+
+    thread.start_new_thread(readModelData, (m))
+
+    # max_acc = 0
+    # max_train_acc = 0
+
+    # max_e = None
+
+    # for e in os.listdir(f"{result_dir}/{m}"):
+
+    #     if e == 'model': continue
+
+    #     res = torch.load(f"{result_dir}/{m}/{e}", map_location=torch.device('cpu'))
+    #     acc = accuracy_score(res['val_actual'], res['val_predicted'])
+    #     train_acc = accuracy_score(res['train_actual'], res['train_predicted'])
+
+    #     if acc > max_acc:
+    #         max_acc = acc
+    #         max_e = e
+    #     if train_acc > max_train_acc:
+    #         max_train_acc = train_acc
+
+    # results[m] = max_acc
+
+    # print(f"Epoch {max_e}")
+    # print(f"Val Accuracy: {max_acc}")
+    # print(f"Train Accuracy: {max_train_acc}")
+
+    # print("____________________________")
 
 s = {k: v for k, v in sorted(results.items(), key=lambda item: item[1])}
 for k in s.keys():
