ac40625eb3c091c00cf84eb6889bea236652354a
NickFleece <nebf97@gmail.com>
2023-03-06 23:04:01 -0500

    Update quick_eval.py

--- skeleton-action-recognition/jhmdb/quick_eval.py
+++ skeleton-action-recognition/jhmdb/quick_eval.py
@@ -4,26 +4,27 @@ import os
 from sklearn.metrics import accuracy_score, confusion_matrix
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-import time
-from threading import Thread
 
 result_dir = "/comm_dat/nfleece/JHMDB/models"
 
 print(f"Analyzing {len(os.listdir(result_dir))} total models...")
 results = {}
 count = 0
+for m in os.listdir(f"{result_dir}"):
 
-def readModelData(modelName):
+    count += 1
+    print(f"{count} - {m}")
+
     max_acc = 0
     max_train_acc = 0
 
     max_e = None
 
-    for e in os.listdir(f"{result_dir}/{modelName}"):
+    for e in os.listdir(f"{result_dir}/{m}"):
 
         if e == 'model': continue
 
-        res = torch.load(f"{result_dir}/{modelName}/{e}", map_location=torch.device('cpu'))
+        res = torch.load(f"{result_dir}/{m}/{e}", map_location=torch.device('cpu'))
         acc = accuracy_score(res['val_actual'], res['val_predicted'])
         train_acc = accuracy_score(res['train_actual'], res['train_predicted'])
 
@@ -34,21 +35,12 @@ def readModelData(modelName):
             max_train_acc = train_acc
 
     results[m] = max_acc
-    print(f"{modelName} DONE")
 
-threads = []
-for m in os.listdir(f"{result_dir}"):
+    print(f"Epoch {max_e}")
+    print(f"Val Accuracy: {max_acc}")
+    print(f"Train Accuracy: {max_train_acc}")
 
-    count += 1
-    print(f"{count} - {m}")
-
-    t = Thread(target=readModelData, args=[m])
-    t.start()
-    threads.append(t)
-    time.sleep(3)
-
-for t in threads:
-    t.join()
+    print("____________________________")
 
 s = {k: v for k, v in sorted(results.items(), key=lambda item: item[1])}
 for k in s.keys():
