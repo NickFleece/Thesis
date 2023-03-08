3088f9d4329a1fd42a200ded22a6ae9af33f0e9b
Nick <nebf97@gmail.com>
2023-03-08 01:05:44 -0500

    fix the bash script

--- skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
+++ skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
@@ -8,9 +8,9 @@ for batchsize in "${BATCH_SIZES[@]}"
 do
     for filters in "${NUM_FILTERS[@]}"
     do
-        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters $filters --version $1-$lr-$filters-$batchsize &
-        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.001 --batch_size $batchsize --num_filters $filters --version $1-$lr-$filters-$batchsize &
-        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.0001 --batch_size $batchsize --num_filters $filters --version $1-$lr-$filters-$batchsize &
+        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters $filters --version $1-0.01-$filters-$batchsize &
+        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.001 --batch_size $batchsize --num_filters $filters --version $1-0.001-$filters-$batchsize &
+        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.0001 --batch_size $batchsize --num_filters $filters --version $1-0.0001-$filters-$batchsize &
         wait
     done
 done 
