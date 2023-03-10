4215215091c811fa4f7904f193cc2178a56e704f
NickFleece <nebf97@gmail.com>
2023-03-10 01:16:42 -0500

    10.1 take 2

--- skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
+++ skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
@@ -11,9 +11,9 @@ do
     do
         python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 32 --weight_decay $weightdecay --gpu 0 --version $1-0.01-32-$batchsize-$weightdecay &
         python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 64 --weight_decay $weightdecay --gpu 0 --version $1-0.01-64-$batchsize-$weightdecay &
-        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 128 --weight_decay $weightdecay --gpu 1 --version $1-0.01-128-$batchsize-$weightdecay &
+        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 128 --weight_decay $weightdecay --gpu 0 --version $1-0.01-128-$batchsize-$weightdecay &
         python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 256 --weight_decay $weightdecay --gpu 0 --version $1-0.01-256-$batchsize-$weightdecay &
-        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 512 --weight_decay $weightdecay --gpu 1 --version $1-0.01-512-$batchsize-$weightdecay &
+        python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 512 --weight_decay $weightdecay --gpu 0 --version $1-0.01-512-$batchsize-$weightdecay &
         python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 1024 --weight_decay $weightdecay --gpu 0 --version $1-0.01-1024-$batchsize-$weightdecay &
         python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 2048 --weight_decay $weightdecay --gpu 1 --version $1-0.01-2048-$batchsize-$weightdecay &
         wait
