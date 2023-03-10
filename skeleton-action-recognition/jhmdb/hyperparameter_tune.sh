700e54a63ce52d20fe53908382999f0f0c7eab77
NickFleece <nebf97@gmail.com>
2023-03-10 01:06:25 -0500

    change script for v10 to change the weight decay

--- skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
+++ skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
@@ -6,17 +6,19 @@ NUM_FILTERS=('32' '64' '128' '256')
 
 for batchsize in "${BATCH_SIZES[@]}"
 do
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 32 --gpu 0 --version $1-0.01-32-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.001 --batch_size $batchsize --num_filters 32 --gpu 1 --version $1-0.001-32-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.0001 --batch_size $batchsize --num_filters 32 --gpu 1 --version $1-0.0001-32-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 64 --gpu 0 --version $1-0.01-64-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.001 --batch_size $batchsize --num_filters 64 --gpu 0 --version $1-0.001-64-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.0001 --batch_size $batchsize --num_filters 64 --gpu 1 --version $1-0.0001-64-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 128 --gpu 0 --version $1-0.01-128-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.001 --batch_size $batchsize --num_filters 128 --gpu 1 --version $1-0.001-128-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.0001 --batch_size $batchsize --num_filters 128 --gpu 1 --version $1-0.0001-128-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 256 --gpu 0 --version $1-0.01-256-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.001 --batch_size $batchsize --num_filters 256 --gpu 0 --version $1-0.001-256-$batchsize &
-    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.0001 --batch_size $batchsize --num_filters 256 --gpu 1 --version $1-0.0001-256-$batchsize &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 32 --weight_decay 0.001 --gpu 0 --version $1-0.01-32-$batchsize-0.001 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 64 --weight_decay 0.001 --gpu 0 --version $1-0.01-64-$batchsize-0.001 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 128 --weight_decay 0.001 --gpu 0 --version $1-0.01-128-$batchsize-0.001 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 256 --weight_decay 0.001 --gpu 0 --version $1-0.01-256-$batchsize-0.001 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 512 --weight_decay 0.001 --gpu 0 --version $1-0.01-512-$batchsize-0.001 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 1024 --weight_decay 0.001 --gpu 0 --version $1-0.01-1024-$batchsize-0.001 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 2048 --weight_decay 0.001 --gpu 0 --version $1-0.01-2048-$batchsize-0.001 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 32 --weight_decay 0.01 --gpu 1 --version $1-0.01-32-$batchsize-0.01 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 64 --weight_decay 0.01 --gpu 1 --version $1-0.01-64-$batchsize-0.01 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 128 --weight_decay 0.01 --gpu 1 --version $1-0.01-128-$batchsize-0.01 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 256 --weight_decay 0.01 --gpu 1 --version $1-0.01-256-$batchsize-0.01 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 512 --weight_decay 0.01 --gpu 1 --version $1-0.01-512-$batchsize-0.01 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 1024 --weight_decay 0.01 --gpu 1 --version $1-0.01-1024-$batchsize-0.01 &
+    python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size $batchsize --num_filters 2048 --weight_decay 0.01 --gpu 1 --version $1-0.01-2048-$batchsize-0.01 &
     wait
 done 
