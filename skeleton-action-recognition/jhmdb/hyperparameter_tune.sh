b7116ea234c00e2284c9e07bfc8154a9c09354fa
NickFleece <nebf97@gmail.com>
2023-03-11 14:35:14 -0500

    change gpu to 1

--- skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
+++ skeleton-action-recognition/jhmdb/hyperparameter_tune.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 128 --gpu 0 --version $1-0.01-128-16 &
-python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 256 --gpu 0 --version $1-0.01-256-16 &
-python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 512 --gpu 0 --version $1-0.01-512-16 &
-python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 1024 --gpu 0 --version $1-0.01-1024-16 &
+python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 128 --gpu 1 --version $1-0.01-128-16 &
+python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 256 --gpu 1 --version $1-0.01-256-16 &
+python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 512 --gpu 1 --version $1-0.01-512-16 &
+python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 1024 --gpu 1 --version $1-0.01-1024-16 &
 wait
