b421db6b1e3a35f14296dad5014b9a5f377a41fe
NickFleece <nebf97@gmail.com>
2023-04-10 18:15:38 -0400

    10.7.1

--- skeleton-action-recognition/jhmdb/tune_weight_decay.sh
+++ skeleton-action-recognition/jhmdb/tune_weight_decay.sh
@@ -1,5 +1,5 @@
-python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 128 --gpu 1 --verbose 2 --weight_decay 0.005 --version $1-0.005-0.01-128-16 &
-python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 128 --gpu 1 --verbose 2 --weight_decay 0.004 --version $1-0.004-0.01-128-16 &
-python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 256 --gpu 1 --verbose 2 --weight_decay 0.005 --version $1-0.005-0.01-256-16 &
-python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 256 --gpu 1 --verbose 2 --weight_decay 0.004 --version $1-0.004-0.01-256-16 &
+python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 512 --gpu 1 --verbose 2 --weight_decay 0.0053 --version $1-0.0055-0.01-512-16 &
+python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 512 --gpu 1 --verbose 2 --weight_decay 0.0052 --version $1-0.0052-0.01-512-16 &
+python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 512 --gpu 1 --verbose 2 --weight_decay 0.0051 --version $1-0.0051-0.01-512-16 &
+python skeleton_model.py --drive_dir /comm_dat/nfleece/JHMDB --learning_rate 0.01 --batch_size 16 --num_filters 512 --gpu 1 --verbose 2 --weight_decay 0.005 --version $1-0.005-0.01-512-16 &
 wait
